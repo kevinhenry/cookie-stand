d39065177e2ae67d3e7d74fd80e5daf8521c39ee
Kevin henry <kevinhenry@mac.com>
2021-01-26 21:52:22 -0800

    Initial commit with scaffolding

---
+++ js/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+console.log('hello world');
+
+// get each store element by id.
+
+// let hours = ['6am', '7am', '8am'];
+
+// let seattle = {
+  // name: 'Seattle',
+  // min: 23,
+  // max: 65,
+  // avg: 6.3,
+  // a method to calculate random number of custers per hour
+  // a method to calculate and populate our number of cookies sold per hour array
+  // a method to render the list items.
+}
+
